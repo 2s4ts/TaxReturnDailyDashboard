3075f326ed6f0b475e3d284e6df20c83c682dec6
Ziv Schneideman <ziv.schneideman@gmail.com>
2026-06-22 18:08:46 +0300

    Connect GitHub Pages site to Apps Script backend

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.DASHBOARD_BACKEND_URL = "";
+window.DASHBOARD_BACKEND_URL = "https://script.google.com/macros/s/AKfycbxU4gTJJ5GEK3G6ALJxSxGE5-lid2Nrcdm0RKJztZ-Qgv69QATNqGUs9aJISvpZ-kuc/exec";
